0d52970202cac918f1d82460317d28a7d1f3b6c2
amavr <albert.mavrin@gmail.com>
2015-04-06 21:37:47 +0300

    Восстановление рабочей версии

---
+++ config/config.js
@@ -0,0 +1,4 @@
+﻿var cfg = {
+    mongourl: "mongodb://timehub:app35552099$@ds061371.mongolab.com:61371/heroku_app35552099",
+    password: ""
+}
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 ﻿var express = require('express');
 var router = express.Router();
 
+//var config = require('./config/config');
+//var mongoose = require('mongoose').Mongoose;
+//db = mongoose.connect(config.conf.mongodburl)
+
 /* GET users listing. */
 router.get('/', function (req, res) {
     res.send('respond with a resource');
